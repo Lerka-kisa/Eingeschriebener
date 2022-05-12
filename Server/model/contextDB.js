2937c18062683ed9083a11934426ee7489a81d8a
Skvortsoff <garikmogilev@gmail.com>
2022-05-12 20:42:11 +0300

    add little bit css

--- Server/model/contextDB.js
+++ Server/model/contextDB.js
@@ -1,17 +1,21 @@
 const Sequelize = require('sequelize');
 const Model = Sequelize.Model;
 
-const sequelize = new Sequelize('Eingeschriebener', "LERA", "Nata_5442488",
-    {host: 'localhost', dialect: 'mssql'}
+const sequelize = new Sequelize(
+    'Eingeschriebener',
+    "root",
+    "9I50ybkubu",
+    {host: 'localhost', dialect: 'mariadb'}
+
 );
 
 let start = setInterval(() =>
 {
     connect();
-}, 1000);
+}, 5000);
 
 
-const connect = () =>  sequelize.sync()
+const connect = () =>  sequelize.sync(/*{force : true}*/)
     .then(() => {
         clearInterval(start);
         console.log(`Connected to Eingeschriebener, host : localhost`) })

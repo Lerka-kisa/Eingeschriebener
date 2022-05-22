39b748f80f920c7b367f0bec05bdf9f4751a316b
Lerka-kisa <troshko_lerka@mail.ru>
2022-05-22 19:07:54 +0300

    Made validate in search page

--- Server/public/js/search.js
+++ Server/public/js/search.js
@@ -1,3 +1,68 @@
+let mathCheck = false
+let physCheck = false
+let langCheck = false
+let attCheck = false
+
+function checkMath(num) {
+    if(num < 0 || num > 100)
+    {
+        document.getElementById("errorMath").innerHTML = "Таких баллов по математике не может быть";
+        document.getElementsByName("math")[0].style.borderColor = "red";
+        mathCheck = false;
+    }
+    else
+    {
+        document.getElementById("errorInput").innerHTML = "";
+        document.getElementsByName("math")[0].style.borderColor = "green";
+        mathCheck = true;
+    }
+}
+
+function checkPhys(num) {
+    if(num < 0 || num > 100)
+    {
+        document.getElementById("errorPhys").innerHTML = "Таких баллов по физике не может быть";
+        document.getElementsByName("physics")[0].style.borderColor = "red";
+        physCheck = false;
+    }
+    else
+    {
+        document.getElementById("errorInput").innerHTML = "";
+        document.getElementsByName("physics")[0].style.borderColor = "green";
+        physCheck = true;
+    }
+}
+
+function checkLang(num) {
+    if(num < 0 || num > 100)
+    {
+        document.getElementById("errorLang").innerHTML = "Таких баллов по языку не может быть";
+        document.getElementsByName("language")[0].style.borderColor = "red";
+        langCheck = false;
+    }
+    else
+    {
+        document.getElementById("errorInput").innerHTML = "";
+        document.getElementsByName("language")[0].style.borderColor = "green";
+        langCheck = true;
+    }
+}
+
+function checkAtt(num) {
+    if(num < 0 || num > 100)
+    {
+        document.getElementById("errorAtt").innerHTML = "Таких баллов за аттестат не может быть";
+        document.getElementsByName("certificate")[0].style.borderColor = "red";
+        attCheck = false;
+    }
+    else
+    {
+        document.getElementById("errorAtt").innerHTML = "";
+        document.getElementsByName("certificate")[0].style.borderColor = "green";
+        attCheck = true;
+    }
+}
+
 const send = () =>{
     const math = document.getElementById("math").value;
     const physics = document.getElementById("physics").value;
@@ -11,31 +76,36 @@ const send = () =>{
         }
     });
 
-    //fetch("https://localhost:443/univers/offer", /*TODO https*/{
-    fetch("https://Eingeschriebener/univers/offer", /*TODO link*/{
-        method : 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify(
-            {
-                math : math,
-                physics: physics,
-                language: language,
-                certificate: certificate,
-                contact: contact
-            })
-    }).then(response => {return response.json()})
-        .then(result => {
-            if(result.error){
-                //console.log(result.error)
-                document.getElementById("container__list").innerText = result.error;
-            }else {
-                document.getElementById("container__list").innerHTML = builderList(result)
-            }
-        }).catch(err => {
-        console.log(err)
-    })
+    if(mathCheck&&physCheck&&langCheck&&attCheck){
+        //fetch("https://localhost:443/univers/offer", /*TODO https*/{
+        fetch("https://Eingeschriebener/univers/offer", /*TODO link*/{
+            method : 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(
+                {
+                    math : math,
+                    physics: physics,
+                    language: language,
+                    certificate: certificate,
+                    contact: contact
+                })
+        }).then(response => {return response.json()})
+            .then(result => {
+                if(result.error){
+                    //console.log(result.error)
+                    document.getElementById("container__list").innerText = result.error;
+                }else {
+                    document.getElementById("container__list").innerHTML = builderList(result)
+                }
+            }).catch(err => {
+            console.log(err)
+        })
+    }
+    else{
+        document.getElementById("error").innerHTML = "Ты глупенький??? Проверь, что ты ввёл";
+    }
 
 }
 const getAll = () =>{
